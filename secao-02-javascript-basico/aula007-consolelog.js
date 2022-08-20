402861714fc134460c856a499eb0d4cf761ec20f
ErickFSilva <erick_ferreira@outlook.com>
2022-08-20 11:48:07 -0300

    secao-02_javascript-basico_20/08/2022

---
+++ secao-02-javascript-basico/aula007-consolelog.js
@@ -0,0 +1,13 @@
+
+// Função console.log
+// Obs.: Método 'log' que está dentro do objeto 'console'
+
+// String
+console.log("Erick Ferreira, 'Programador Web'");
+console.log(`"Erick Ferreira", 'Full Stack'`);
+
+// Número literal
+console.log('Números literais: ', 123456);
+
+// Número com ponto flutuante
+console.log('Números com ponto flutuante: ', 1234.56)
---
+++ secao-02-javascript-basico/aula008-exercicios.js
@@ -0,0 +1,3 @@
+console.log(
+    'Meu nome é "Erick". Estou aprendendo JavaScript às', 7, 'da manhã.'
+)
