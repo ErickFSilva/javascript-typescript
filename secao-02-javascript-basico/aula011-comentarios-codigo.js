1ad9d3f1365f0fa5d09628bfc1fa3fac98b707a6
ErickFSilva <erick_ferreira@outlook.com>
2022-08-20 12:22:58 -0300

    secao-02_javascript-basico_20/08/2022(3)

---
+++ secao-02-javascript-basico/aula011-comentarios-codigo.js
@@ -0,0 +1,16 @@
+
+// Este é um comentário de uma linha
+
+console.log('Olá, Erick!'); // Aqui também pode ter comentário de linha
+console.log('Linha 2');
+// console.log('Linha 3'); (Comentando a linha de código)
+console.log('Linha 4');
+
+
+/**
+ * Comentário de bloco:
+ * 
+ * Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Suspendisse sed consectetur tortor.
+ * Interdum et malesuada fames ac ante ipsum primis in faucibus.
+ */
