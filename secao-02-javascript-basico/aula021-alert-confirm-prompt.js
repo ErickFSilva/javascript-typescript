584552956ed1d54bbf278d12786ebda2bc087759
ErickFSilva <erick_ferreira@outlook.com>
2022-10-19 16:40:52 -0300

    secao-02_javascript-basico_19/10/2022

--- secao-02-javascript-basico/aula021-alert-confirm-prompt.js
+++ secao-02-javascript-basico/aula021-alert-confirm-prompt.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-let num1 = parseFloat(prompt('Digite um número:'));
-let num2 = parseFloat(prompt('Digite outro número:'));
+console.log('Olá!')
 
-let total = num1 + num2;
-
-alert('Total: ' + total);
-=======
-let num1 = parseInt(prompt('Digite um número'));
-let num2 = parseInt(prompt('Digite outro número'));
-
-alert(`O resultado da sua conta foi: ${num1 + num2}`);
->>>>>>> 50d1ad37cb753c2afb902cbd482b82331001a746
+alert('Olá!')
---
+++ secao-02-javascript-basico/aula024-mais-sobre-strings.js
@@ -0,0 +1,25 @@
+// índices:    0123456789...
+let string1 = "o rato roeu a roupa do rei de roma!";
+let string2 = 'Outro Texto';
+
+console.log(string1);
+console.log(string2);
+console.log(string1[6]);
+console.log(string1.charAt(6));
+console.log(string1.charAt(60));
+console.log(string1.concat(string2)); // concatena strings
+console.log(`${string1} ${string2}`); // concatena strings
+console.log(string1.indexOf('texto', 3)); // encontra a posição do texto em pesquisa, apartir da posição informada
+console.log(string1.indexOf('texto')); // encontra a posição do texto em pesquisa
+console.log(string2.lastIndexOf('e')); // encontra a posição do texto em pesquisa
+console.log(string1.match(/[a-z]/g)); // ?
+console.log(string1.search(/texto/)); // encontra o texto em pesquisa
+console.log(string1.replace('o', 'O')); // substitui palavras
+console.log(string1.replace(/r/g, 'R')); // substitui palavras
+console.log(string1.length); // conta a quantidade de caracteres da string
+console.log(string1.slice(2, 11)); // extrai parte da string na posição informada
+console.log(string1.slice(-5, -1)); // extrai parte da string na posição informada
+console.log(string1.split(' ')); // divide o string pelo caractere informado, criando um array com as posições totais
+console.log(string1.split(' ', 3)); // divide o string pelo caractere informado, criando um array com as posições informadas
+console.log(string2.toUpperCase()); // converte os caracteres para maiúculos
+console.log(string2.toLowerCase()); // converte os caracteres para minúsculos
