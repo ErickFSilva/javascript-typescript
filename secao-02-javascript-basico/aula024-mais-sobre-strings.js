8b17bc14b1b6fd605fc48fe21a766feb98100f5f
ErickFSilva <erick_ferreira@outlook.com>
2022-10-23 19:34:04 -0300

    secao-02_javascript-basico_23/10/2022

--- secao-02-javascript-basico/aula024-mais-sobre-strings.js
+++ secao-02-javascript-basico/aula024-mais-sobre-strings.js
@@ -2,16 +2,15 @@
 let string1 = "o rato roeu a roupa do rei de roma!";
 let string2 = 'Outro Texto';
 
-console.log(string1);
-console.log(string2);
-console.log(string1[6]);
-console.log(string1.charAt(6));
-console.log(string1.charAt(60));
+console.log(string1); // imprime o string
+console.log(string2); // imprime o string
+console.log(string1[2]); // imprime o caractere na posição informada
+console.log(string1.charAt(4)); // imprime o caractere na posição informada
 console.log(string1.concat(string2)); // concatena strings
 console.log(`${string1} ${string2}`); // concatena strings
 console.log(string1.indexOf('texto', 3)); // encontra a posição do texto em pesquisa, apartir da posição informada
-console.log(string1.indexOf('texto')); // encontra a posição do texto em pesquisa
-console.log(string2.lastIndexOf('e')); // encontra a posição do texto em pesquisa
+console.log(string1.indexOf('texto')); // encontra a posição, inicial, do texto em pesquisa
+console.log(string2.lastIndexOf('e')); // encontra a posição, final, do texto em pesquisa
 console.log(string1.match(/[a-z]/g)); // ?
 console.log(string1.search(/texto/)); // encontra o texto em pesquisa
 console.log(string1.replace('o', 'O')); // substitui palavras
